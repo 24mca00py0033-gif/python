ee31bc533201bd139cf2b7f7ea9a8115f7d6954b
24mca00py0033-gif <24mca00py0033@pondiuni.ac.in>
2025-12-20 15:45:04 +0530

    comit

--- lsit/list2.py
+++ lsit/list2.py
@@ -1,27 +1,47 @@
-squres =[]
-for i in range(1,11):
-    squres.append(i*i)
+# squres =[]
+# for i in range(1,11):
+#     squres.append(i*i)
 
-print(squres)
+# print(squres)
 
-squres=[i**2 for i in range (1,11)]
-print(squres)
-
-
-strings=["apple","banana","cherry","date"]
-
-lengts=[len(s) for s in strings]
-print(lengts)
+# squres=[i**2 for i in range (1,11)]
+# print(squres)
 
 
-olist=["a","b","c"]
+# strings=["apple","banana","cherry","date"]
 
-repeated=[item *3 for item in olist ]
-print(repeated) 
+# lengts=[len(s) for s in strings]
+# print(lengts)
 
-numbers=[ i for i in range(1,21)]
 
-result=[[num,"EVEN" if num %2 ==0 else "ODD"] for num in numbers]
+# olist=["a","b","c"]
 
-print(result)
+# repeated=[item *3 for item in olist ]
+# print(repeated) 
 
+# numbers=[ i for i in range(1,21)]
+
+# result=[[num,"EVEN" if num %2 ==0 else "ODD"] for num in numbers]
+
+# print(result)
+
+# mylist=[1,2,4,5,6,7]
+# print(mylist[::-1])# for reversing list using slicing 
+
+# my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# print(my_list[::3])
+
+# middle=len(my_list)//2
+
+# print(my_list[:middle])
+# print(my_list[middle:])
+
+# my_list = ["a", "b", "c", "d", "e", "f", "g"]
+# n = 3
+
+# # Use a negative index to start 'n' steps from the end
+# last_n_elements = my_list[-n:]
+
+# print(f"The last {n} elements are:", last_n_elements)
+# # Output: The last 3 elements are: ['e', 'f', 'g']  
