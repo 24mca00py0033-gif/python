4dc15f021e1fd9b7f0211522c5431cef1fd74d11
24mca00py0033-gif <24MCA00PY0033@PONDI.UNI.AC.IN>
2026-02-03 09:24:23 +0530

    daily commit

--- day 2/reverseanarray.py
+++ day 2/reverseanarray.py
@@ -1,9 +1,23 @@
-def reverse_arr(arr, left, right):
-    if left >= right:  
-        return
-    arr[left], arr[right] = arr[right], arr[left] 
-    reverse_arr(arr, left + 1, right - 1)
+# def reverse_arr(arr, left, right):
+#     if left >= right:  
+#         return
+#     arr[left], arr[right] = arr[right], arr[left] 
+#     reverse_arr(arr, left + 1, right - 1)
 
 arr = [1, 2, 3, 4, 5]
-reverse_arr(arr, 0, len(arr) - 1)
-print(arr)
+# reverse_arr(arr, 0, len(arr) - 1)
+# print(arr)
+# print(arr[::-1]) through slicing 
+
+
+def reverse_arr(arr):
+    left=0
+    right=len(arr)-1
+
+    while left<=right:
+        arr[left],arr[right]=arr[right],arr[left]
+        left+=1
+        right-=1
+
+    return arr
+print(reverse_arr(arr))
